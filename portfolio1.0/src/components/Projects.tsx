0a7b8b2cdb011007d844d5590cf2d3de490a0e71
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-07-10 16:39:52 +0200

    wip projects

--- portfolio1.0/src/components/Projects.tsx
+++ portfolio1.0/src/components/Projects.tsx
@@ -6,19 +6,53 @@ import projectFour from "./images/Screenshot 2024-02-01 194213.png";
 
 export const Projects = () => {
   return (
-    <div className="projectContainer">
-      <div className="projectBox1">
-        <img src={projectOne} alt="projectOne" />
-      </div>
-      <div className="projectBox2">
-        <img src={projectTwo} alt="projectTwo" />
-      </div>
-      <div className="projectBox3">
-        <img src={projectThree} alt="projectThree" />
-      </div>
-      <div className="projectBox4">
-        <img src={projectFour} alt="projectFour" />
+    <div className="projectPageContainer">
+      <div className="projectContainer">
+        <div className="galleryContainer w4">
+          <div className="galleryItem">
+            <div className="image">
+              <img src={projectOne} alt="projectOne" />
+            </div>
+          </div>
+        </div>
+        <div className="galleryContainer w2">
+          <div className="galleryItem">
+            <div className="image">
+              <img src={projectTwo} alt="projectTwo" />
+            </div>
+          </div>
+        </div>
+        <div className="galleryContainer w2">
+          <div className="galleryItem">
+            <div className="image">
+              <img src={projectThree} alt="projectThree" />
+            </div>
+          </div>
+        </div>
+        <div className="galleryContainer w4">
+          <div className="galleryItem">
+            <div className="image">
+              <img src={projectFour} alt="projectFour" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    // <div className="projectPageContainer">
+    //   <div className="projectContainer">
+    //     <div className="projectBox1">
+    //       <img src={projectOne} alt="projectOne" />
+    //     </div>
+    //     <div className="projectBox2">
+    //       <img src={projectTwo} alt="projectTwo" />
+    //     </div>
+    //     <div className="projectBox3">
+    //       <img src={projectThree} alt="projectThree" />
+    //     </div>
+    //     <div className="projectBox4">
+    //       <img src={projectFour} alt="projectFour" />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
