c68002ee1d1c010afcd27033801751770c6bf81e
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2026-01-24 14:41:46 +0100

    new portfolio link

--- portfolio1.0/src/components/Projects.tsx
+++ portfolio1.0/src/components/Projects.tsx
@@ -49,7 +49,7 @@ export const Projects = () => {
       description:
         "Curious about how this portfolio was built? This repo contains the full code behind the site you're currently viewing.",
       github: "https://github.com/SophiaKornilia/Portfolio1.0",
-      live: "https://vercel.com/kornilias-projects/portfolio1-0",
+      live: "https://portfolio1-0-plmrm49tk-kornilias-projects.vercel.app/",
       tech: ["React", "TypeScript", "CSS"],
     },
   ];
